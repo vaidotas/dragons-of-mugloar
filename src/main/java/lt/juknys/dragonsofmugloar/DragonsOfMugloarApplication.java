16a433e132a744f855fd30481be708a4a03d0bd5
Vaidotas Juknys <vaidotas.juknys@visma.com>
2017-12-15 10:20:41 +0200

    Initial commit

---
+++ src/main/java/lt/juknys/dragonsofmugloar/DragonsOfMugloarApplication.java
@@ -0,0 +1,12 @@
+package lt.juknys.dragonsofmugloar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DragonsOfMugloarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DragonsOfMugloarApplication.class, args);
+	}
+}
